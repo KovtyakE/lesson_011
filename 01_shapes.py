05a84446f84be1bbc0266191f142c7ec5d41d0f8
EVG <evgeniy-kovtyak@mail.ru>
2021-12-06 13:09:55 +0300

    01 completed. Simple fabric-function.

--- 01_shapes.py
+++ 01_shapes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+sd.set_screen_size(1200, 600)
 
 # На основе вашего кода из решения lesson_004/01_shapes.py сделать функцию-фабрику,
 # которая возвращает функции рисования треугольника, четырехугольника, пятиугольника и т.д.
@@ -14,12 +15,19 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def draw_polygon(point, angle=0):
+        length = 400 / n
+        every_angle = 360 / n
+        start_point = point
+        for iteration in range(n):
+            line = sd.get_vector(start_point=start_point, angle=angle, length=length, width=1)
+            line.draw()
+            start_point = line.end_point
+            angle += every_angle
+            # sd.sleep(0.1)
+    return draw_polygon
 
 
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
-
+draw_triangle = get_polygon(n=4)
+draw_triangle(point=sd.get_point(600, 100), angle=15)
 sd.pause()
